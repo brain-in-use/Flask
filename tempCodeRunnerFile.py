4bf4b1517248d8048c34e1ca7c073c873855298f
brain-in-use <senaakash008@gmail.com>
2023-02-08 21:58:39 +0530

    Initial commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+@app.route('/delete/<int:sno>')
+# def delete():
+#     todo=Todo.query.filter_by(sno=sno).first()
+#     db.session.delete(todo)
+#     db.session.commit()
+#     return redirect('/')
